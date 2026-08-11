5dd9c4b8d5b3adc20fdaa161cb697c0510923c69
PigOrr_ <121391862+Pig0rr@users.noreply.github.com>
2026-08-11 15:45:27 +0300

    Update BackpackScreen.java

--- src/main/java/com/spydnel/backpacks/client/gui/BackpackScreen.java
+++ src/main/java/com/spydnel/backpacks/client/gui/BackpackScreen.java
@@ -11,8 +11,6 @@ public class BackpackScreen extends AbstractContainerScreen<BackpackMenu> {
     private static final ResourceLocation TEXTURE =
             ResourceLocation.withDefaultNamespace("textures/gui/container/generic_54.png");
     private static final int PANEL_COLOR = 0xFFC6C6C6;
-    private static final int SLOT_BORDER_COLOR = 0xFF373737;
-    private static final int SLOT_FILL_COLOR = 0xFF8B8B8B;
 
     public BackpackScreen(BackpackMenu menu, Inventory playerInventory, Component title) {
         super(menu, playerInventory, title);
@@ -27,20 +25,20 @@ public class BackpackScreen extends AbstractContainerScreen<BackpackMenu> {
         int y = (this.height - this.imageHeight) / 2;
         int topHeight = 17 + this.menu.rows * 18;
 
-        guiGraphics.blit(TEXTURE, x, y, 0, 0, this.imageWidth, 17);
-
-        guiGraphics.fill(x + 1, y + 17, x + this.imageWidth - 1, y + topHeight, PANEL_COLOR);
+        guiGraphics.blit(TEXTURE, x, y, 0, 0, this.imageWidth, topHeight);
 
         for (int row = 0; row < this.menu.rows; row++) {
             int slotsInRow = Math.min(9, this.menu.slotCount - row * 9);
+            int hiddenCols = 9 - slotsInRow;
+            int hiddenLeft = hiddenCols / 2;
+            int hiddenRight = hiddenCols - hiddenLeft;
             int rowY = y + 17 + row * 18;
-            int rowWidth = slotsInRow * 18;
-            int rowX = x + (this.imageWidth - rowWidth) / 2;
 
-            for (int col = 0; col < slotsInRow; col++) {
-                int slotX = rowX + col * 18;
-                guiGraphics.fill(slotX, rowY, slotX + 18, rowY + 18, SLOT_BORDER_COLOR);
-                guiGraphics.fill(slotX + 1, rowY + 1, slotX + 17, rowY + 17, SLOT_FILL_COLOR);
+            if (hiddenLeft > 0) {
+                guiGraphics.fill(x + 7, rowY, x + 7 + hiddenLeft * 18, rowY + 18, PANEL_COLOR);
+            }
+            if (hiddenRight > 0) {
+                guiGraphics.fill(x + 7 + (9 - hiddenRight) * 18, rowY, x + 7 + 9 * 18, rowY + 18, PANEL_COLOR);
             }
         }
 
